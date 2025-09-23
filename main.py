855a2e6be50fc7ac3143d18def87853cc56ed5ac
Lugia11 <noahgunnarsson2011@hotmail.com>
2025-09-23 17:39:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,68 @@
+from js import document
 import time
 import random
-from js import document
 
+# Set variables random value
+x = random.randint(1, 6)
+y = random.randint(1, 3)
+z = random.randint(2, 5)
 
-#Set variables random value
-x = random.randint (1,6)
-y = random.randint (1,3)
-z = random.randint (2,5)
-
-#Prints the value of the variables
-
+# Prints the value of the variables
 if z == 2:
     time.sleep(0.25)
     output_div = document.querySelector("#rolling")
-    output_div.innerText "Rolling dice"
+    output_div.textContent = "Rolling dice"
     time.sleep(0.5)
     output_div = document.querySelector("#rolling")
-    output_div.innerText "Rolling dice."
+    output_div.textContent = "Rolling dice."
     time.sleep(0.5)
     output_div = document.querySelector("#roll")
-    output_div.innerText f"You rolled a {x}"
+    output_div.textContent = f"You rolled a {x}"
 elif z == 3:
     time.sleep(0.25)
     output_div = document.querySelector("#rolling")
-    output_div.innerText "Rolling dice"
+    output_div.textContent = "Rolling dice"
     time.sleep(0.5)
     output_div = document.querySelector("#rolling")
-    output_div.innerText "Rolling dice."
+    output_div.textContent = "Rolling dice."
     time.sleep(0.5)
     output_div = document.querySelector("#rolling")
-    output_div.innerText "Rolling dice.."
+    output_div.textContent = "Rolling dice.."
     time.sleep(0.5)
     output_div = document.querySelector("#roll")
-    output_div.innerText f"You rolled a {x}"
+    output_div.textContent = f"You rolled a {x}"
 elif z == 4:
     time.sleep(0.25)
     output_div = document.querySelector("#rolling")
-    output_div.innerText "Rolling dice"
+    output_div.textContent = "Rolling dice"
     time.sleep(0.5)
     output_div = document.querySelector("#rolling")
-    output_div.innerText "Rolling dice."
+    output_div.textContent = "Rolling dice."
     time.sleep(0.5)
     output_div = document.querySelector("#rolling")
-    output_div.innerText "Rolling dice.."
+    output_div.textContent = "Rolling dice.."
     time.sleep(0.5)
     output_div = document.querySelector("#rolling")
-    output_div.innerText "Rolling dice..."
+    output_div.textContent = "Rolling dice..."
     time.sleep(0.5)
     output_div = document.querySelector("#roll")
-    output_div.innerText f"You rolled a {x}"
+    output_div.textContent = f"You rolled a {x}"
 elif z == 5:
     time.sleep(0.25)
     output_div = document.querySelector("#rolling")
-    output_div.innerText "Rolling dice"
+    output_div.textContent = "Rolling dice"
     time.sleep(0.5)
     output_div = document.querySelector("#rolling")
-    output_div.innerText "Rolling dice."
+    output_div.textContent = "Rolling dice."
     time.sleep(0.5)
     output_div = document.querySelector("#rolling")
-    output_div.innerText "Rolling dice.."
+    output_div.textContent = "Rolling dice.."
     time.sleep(0.5)
     output_div = document.querySelector("#rolling")
-    output_div.innerText "Rolling dice..."
+    output_div.textContent = "Rolling dice..."
     time.sleep(0.5)
     output_div = document.querySelector("#rolling")
-    output_div.innerText "Rolling dice"
+    output_div.textContent = "Rolling dice"
     time.sleep(0.5)
     output_div = document.querySelector("#roll")
-    output_div.innerText f"You rolled a {x}"
+    output_div.textContent = f"You rolled a {x}"
